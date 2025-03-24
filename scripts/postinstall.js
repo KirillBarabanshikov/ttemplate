0dc2a95c309bb881b3ecfd59d2600852a2aeedff
Kirill <kirillbar24@gmail.com>
2025-03-24 21:12:58 +0400

    fix

--- scripts/postinstall.js
+++
@@ -1,8 +0,0 @@
-import { rmSync } from 'fs';
-
-try {
-  rmSync('./README.md');
-  console.log('✅ Template initialized!');
-} catch (error) {
-  console.error('❌ Error during postinstall:', error);
-}
